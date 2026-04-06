a79fe9d5bb1f0cb5204f815fa203bb3831362317
mamonova121174-hue <mamonova121174@gmail.com>
2026-04-06 04:16:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  // ЗАМЕНИ НА ИМЯ СВОЕГО РЕПОЗИТОРИЯ
+  base: '/stickermind-ai/', 
   plugins: [react()],
   resolve: {
     alias: {
