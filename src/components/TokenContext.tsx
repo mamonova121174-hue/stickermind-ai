f17523be6d90b2ec2ff17ba601f873291171d452
mamonova121174-hue <mamonova121174@gmail.com>
2026-04-06 03:37:19 +0300

    Update TokenContext.tsx

--- src/components/TokenContext.tsx
+++ src/components/TokenContext.tsx
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 interface TokenContextType {
