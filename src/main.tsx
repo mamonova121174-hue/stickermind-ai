ae08013012887079f3104c915a1d95f0a9676ee4
mamonova121174-hue <mamonova121174@gmail.com>
2026-04-10 03:03:16 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
-alert("REACT ЗАРЯЖЕН");
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+}
